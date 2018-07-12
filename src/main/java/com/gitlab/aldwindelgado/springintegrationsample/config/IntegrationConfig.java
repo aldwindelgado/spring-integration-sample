6466f6e59a5c7dcd4e567134f60bc07d250a4852
Aldwin Paolo A. Delgado <aldwin.paolo.delgado@entrego.com.ph>
2018-07-12 13:08:49 +0800

    UDEMY [Sec. 4 Lect. 19]: Implemented publish subscribe channel

--- src/main/java/com/gitlab/aldwindelgado/springintegrationsample/config/IntegrationConfig.java
+++ src/main/java/com/gitlab/aldwindelgado/springintegrationsample/config/IntegrationConfig.java
@@ -3,8 +3,10 @@ package com.gitlab.aldwindelgado.springintegrationsample.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.channel.DirectChannel;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.integration.channel.PublishSubscribeChannel;
 import org.springframework.integration.config.EnableIntegration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 /**
  * @author Aldwin Delgado
@@ -15,10 +17,15 @@ import org.springframework.integration.config.EnableIntegration;
 public class IntegrationConfig {
 
     @Bean
-    public DirectChannel inputChannel() {
-        DirectChannel directChannel = new DirectChannel();
-        directChannel.setFailover(true);
-        directChannel.setMaxSubscribers(2);
-        return directChannel;
+    public PublishSubscribeChannel inputChannel() {
+        return new PublishSubscribeChannel(customTaskExecutor());
+    }
+
+    @Bean
+    public TaskExecutor customTaskExecutor() {
+        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(10);
+        return executor;
     }
 }
--- src/main/java/com/gitlab/aldwindelgado/springintegrationsample/service/PrintService.java
+++ src/main/java/com/gitlab/aldwindelgado/springintegrationsample/service/PrintService.java
@@ -5,9 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Headers;
-import org.springframework.messaging.support.MessageBuilder;
 
 /**
  * @author Aldwin Delgado
@@ -20,16 +20,15 @@ public class PrintService {
 
     @ServiceActivator(inputChannel = "inputChannel")
     public Message<?> print(Message<String> message, @Headers Map<String, Object> headers) {
-        throw new RuntimeException("This is an error from print service");
-//        log.info("[###] Payload: {}", message.getPayload());
-////        log.info("[###] Headers Params: {}", headers);
-//        int messageNumber = Integer.class.cast(message.getHeaders().get("X-MESSAGE-NUMBER"));
-//        int counterNumber = Integer.class.cast(message.getHeaders().get("X-COUNTER"));
-//        return MessageBuilder
-//            .withPayload(String
-//                .format("Sending a reply for %s with message number of %s", counterNumber,
-//                    messageNumber))
-//            .build();
+        log.info("[###] Payload: {}", message.getPayload());
+//        log.info("[###] Headers Params: {}", headers);
+        int messageNumber = Integer.class.cast(message.getHeaders().get("X-MESSAGE-NUMBER"));
+        int counterNumber = Integer.class.cast(message.getHeaders().get("X-COUNTER"));
+        return MessageBuilder
+            .withPayload(String
+                .format("Sending a reply for %s with message number of %s", counterNumber,
+                    messageNumber))
+            .build();
     }
 
 }
